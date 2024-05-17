75597c6ab5f58721ac63f48b45dc629328d1fef5
bzenky <bzenky@gmail.com>
2024-05-16 21:45:26 -0300

    feat: get user meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,7 +1,11 @@
-import { FastifyInstance } from 'fastify'
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { knexClient } from '../database'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
+
+interface Params {
+  userId: string
+}
 
 export async function mealsRoutes(app: FastifyInstance) {
   const createMealCheckInSchema = z.object({
@@ -12,7 +16,23 @@ export async function mealsRoutes(app: FastifyInstance) {
     userId: z.string()
   })
 
-  app.post('/meals', async (request, reply) => {
+  app.get('/meals/:userId', async (request: FastifyRequest<{ Params: Params }>, reply: FastifyReply) => {
+    const userId = request.params.userId
+
+    const isUserRegistered = await knexClient('users').where('id', userId).first()
+
+    if (!isUserRegistered) {
+      return reply.status(400).send({
+        message: 'User not registered'
+      })
+    }
+
+    const meals = await knexClient('meals').where('user_id', userId)
+
+    return meals
+  })
+
+  app.post('/meals', async (request: FastifyRequest, reply: FastifyReply) => {
     const {
       name,
       description,
